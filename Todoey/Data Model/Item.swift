54eb1e7b045dea15f24047c19252c6ac28c9faf3
Babur Cinoglu <vendetta1987@hotmail.com>
2018-03-29 13:10:48 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Babur Cinoglu on 28.03.2018.
+//  Copyright © 2018 Babur Cinoglu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
+
